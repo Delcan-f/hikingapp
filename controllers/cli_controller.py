3a55e07cd2a46751fe94f6d49a26b42a09e7e868
Delcan-f <109348946+Delcan-f@users.noreply.github.com>
2024-12-15 23:45:37 +1100

    added env files

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -1,3 +1,20 @@
 from flask import Blueprint
 
 from init import db
+
+db_commands = Blueprint("db", __name__)
+
+@db_commands.cli.command("create")
+def create_tables():
+    db.create_all()
+    print("Tables created")
+
+@db_commands.cli.command("drop")
+def drop_tables():
+    db.drop_all()
+    print("Tables dropped")
+
+@db_commands.cli.command("seed")
+def seed_tables():
+
+    pass
