8afcfc55bfa78939a5aeee16fb0fd9d08326a549
Delcan-f <109348946+Delcan-f@users.noreply.github.com>
2024-12-17 22:45:31 +1100

    completed trail equipment model and controller

---
+++ controllers/trail_equipment_controller.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, request
+
+from init import db
+from models.trail_equipment import TrailEquipment, trail_equipment_schema, trail_equipments_schema
+
+trail_equipments_bp = Blueprint("trail_equipments", __name__, url_prefix="/trail_equipments")
+
+# Read one
+@trail_equipments_bp.route("/int:trail_equipment_id>")
+def get_trail_equipment(trail_equipment_id):
+    stmt = db.select(TrailEquipment).filter_by(id=trail_equipment_id)
+    trail_equipment = db.session.scalar(stmt)
+    if trail_equipment:
+        return trail_equipment_schema.dump(trail_equipment)
+    else: # finish writing error response 
+        return {"message": f"write something here"}
+    
+# Read all
+@trail_equipments_bp.route("/")
+def get_trail_equipments():
+    stmt = db.select(TrailEquipment)
+    trail_equipment_list = db.session.scalars(stmt)
+    return trail_equipments_schema.dump(trail_equipment_list)
+
+# Create trail equipment
+@trail_equipments_bp.route("/<int:trail_equipment_id>", methods=["POST"])
+def create_trail_equipment():
+    body_data = trail_equipment_schema.load(request.get_json())
+    trail_equipment = TrailEquipment(
+        required_equipment=body_data.get("required_equipment"),
+        recommended_equipment=body_data.get("recommended_equipment"),
+        hiker_id=body_data.get("hiker_id"),
+        trail_id=body_data.get("trail_id")
+    )
+    db.session.add(trail_equipment)
+    db.session.commit()
+    return trail_equipment_schema.dump(trail_equipment), 201
+
+# Delete trail equipment 
+@trail_equipments_bp.route("/<int:trail_equipment_id>", methods=["DELETE"])
+def delete_trail_equipment(trail_equipment_id):
+    stmt = db.select(TrailEquipment).filter_by(id=trail_equipment_id)
+    trail_equipment = db.session.scalar(stmt)
+    if trail_equipment:
+        db.session.delete(trail_equipment)
+        db.session.commit()
+        return {"message": f"Trail equipment written by {trail_equipment.hiker_id} about trail {trail_equipment.trail_id} has been deleted successfully"}
+    else: 
+        return {"message": f"Trail equipment written by {trail_equipment.hiker_id} about trail {trail_equipment.trail_id} does not exist"}
+    
+# Update trail equipment
+@trail_equipments_bp.route("/<int:trail_equipment_id>", methods=["PUT", "PATCH"])
+def update_trail_equipment(trail_equipment_id):
+    stmt = db.select(TrailEquipment).filter_by(id=trail_equipment_id)
+    trail_equipment = db.session.scalar(stmt)
+    body_data = trail_equipment_schema.load(request.get_json(), partail=True)
+    if trail_equipment:
+        trail_equipment.required_equipment = body_data.get("required_equipment") or trail_equipment.required_equipment
+        trail_equipment.recommended_equipment = body_data.get("recommended_equipment") or trail_equipment.recommended_equipment
+        trail_equipment.hiker_id = body_data.get("hiker_id") or trail_equipment.hiker_id
+        trail_equipment.trail_id = body_data.get("trail_id") or trail_equipment.trail_id
+        db.session.commit()
+        return trail_equipment_schema.dump(trail_equipment)
+    else: 
+        return {"message": f"Trail equipment about trail {trail_equipment.trail_id} does not exist"}, 404
---
+++ models/trail_equipment.py
@@ -0,0 +1,21 @@
+from init import db, ma
+
+class TrailEquipment(db.Model):
+    __tablename__ = "trail_equipment"
+
+    id = db.Column(db.Integer, primary_key=True)
+    required_equipment = db.Column(db.String(100), nullable=False)
+    recommended_equipment = db.Column(db.String(100), nullable=True)
+
+    hiker_id = db.Column(db.Integer, db.ForeignKey("hiker.id"), nullable=False)
+    trail_id = db.Column(db.Integer, db.ForeignKey("trail.id"), nullable=False)
+
+    hiker_id = db.relationship("Hikers", back_populates="trail_equipments")
+    trail_id = db.relationship("Trails", back_populates="trail_equipments")
+
+class TrailEquipmentSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "required_equipment", "recommended_equipment", "hiker_id", "trail_id", "hiker", "trail")
+
+trail_equipment_schema = TrailEquipmentSchema()
+trail_equipments_schema = TrailEquipmentSchema(many=True)
